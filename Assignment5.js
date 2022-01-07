7b947a4bb8f1ec72a9ee2ae669e15f1f4d5dabc9
Yongqi10 <q865984106@yahoo.com>
2022-01-07 18:10:46 -0500

    ChangeAllWhiteT0AnotherColor

--- Assignment5.js
+++ Assignment5.js
@@ -1,20 +1,72 @@
+
+function giveAllBoxClick(){
+    let givingColor = document.querySelectorAll('.box');
+    const givingColorByClick =document.querySelector('#givingColorByClick');
+
+    for(let i = 0; i < givingColor.length; i++)
+    {
+        givingColor[i].addEventListener('click',function(e) {
+    
+            e.target.style.backgroundColor = givingColorByClick.options[givingColorByClick.selectedIndex].value;
+        
+        });
+    }
+    
+}
+
+function ChangeAllWhiteTOAnotherColor() {
+    let givingColor = document.querySelectorAll('.box');
+    const changeAllIfIsWhite = document.querySelector('#changeAllIfIsWhite');
+    const changeColor = document.querySelector('.changeColor');
+
+    changeColor.addEventListener('click', function() {
+
+      for(let i = 0; i < givingColor.length; i++)
+      {
+           if(givingColor[i].style.backgroundColor === '')
+            {
+                givingColor[i].style.backgroundColor = changeAllIfIsWhite.options[changeAllIfIsWhite.selectedIndex].value;
+            }
+    
+     }   
+
+    });
+
+}
+
 const addRowBtn = document.querySelector('.add-row');
 const content = document.querySelector('.content');
 
 
+giveAllBoxClick();
+ChangeAllWhiteTOAnotherColor();
+
 addRowBtn.addEventListener('click', function() {
+
+
+
     const newRow = document.createElement('div');
     newRow.setAttribute('class','row');
-    const numOfCol = document.querySelector('.row').querySelectorAll('.box');
-
-    for(let i = 0; i < numOfCol.length; i++)
+    //if on row in the content add on row to it
+    if(content.children.length == 0)
     {
         const newColumn = document.createElement('div');
         newColumn.setAttribute('class','col-sm border border-dark box white');
         newRow.appendChild(newColumn);
     }
-    
+    else
+    {
+        const numOfCol = document.querySelector('.row').querySelectorAll('.box');
+        for(let i = 0; i < numOfCol.length; i++)
+        {
+            const newColumn = document.createElement('div');
+            newColumn.setAttribute('class','col-sm border border-dark box white');
+            newRow.appendChild(newColumn);
+        }
+    }
     content.appendChild(newRow);
+    giveAllBoxClick();
+    ChangeAllWhiteTOAnotherColor();
 });
 
 const removeRowBtn = document.querySelector('.del-row');
@@ -22,6 +74,10 @@ const removeRowBtn = document.querySelector('.del-row');
 
 removeRowBtn.addEventListener('click', function () {
     //remove last row
+    if(content.children.length == 0)
+    {
+        return;
+    }
     let last = content.children.length - 1;
     content.removeChild(content.children[last]);
     
@@ -38,37 +94,40 @@ addCol.addEventListener('click', function () {
         newColumn.setAttribute('class','col-sm border border-dark box white');
         content.children[i].appendChild(newColumn);
     }
+    giveAllBoxClick();
+    ChangeAllWhiteTOAnotherColor();
+    
 });
 
 const removeColBtn = document.querySelector('.del-col');
 
 removeColBtn.addEventListener('click', function () {
-
     let totalRow = content.children.length;
+    if(content.children[totalRow - 1].children.length == 0)
+    {
+        return;
+    }
+
     //content children = row, row children col
     //get the col for last row
     let lastCol = content.children[totalRow - 1].children.length - 1;
-    console.log(lastCol);
     for(let i = 0; i < totalRow;i++)
     {
         let row = content.children[i];
         row.removeChild(row.children[lastCol])
     }
+    
 });
 
 
-const givingColor = document.querySelectorAll('.box');
-const givingColorByClick =document.querySelector('#givingColorByClick');
 
 
-for(let i = 0; i < givingColor.length; i++)
-{
-    givingColor[i].addEventListener('click',function(e) {
 
-        e.target.style.backgroundColor = givingColorByClick.options[givingColorByClick.selectedIndex].value;
-    
-    });
-}
+
+
+ 
+
+
 
 
 
