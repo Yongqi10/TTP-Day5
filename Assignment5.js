dd163698b0206a8b4e968840e7cd2b894b2d3e83
Yongqi10 <q865984106@yahoo.com>
2022-01-07 15:14:31 -0500

    finish add row

---
+++ Assignment5.js
@@ -0,0 +1,23 @@
+const addRowBtn = document.querySelector('.add-row');
+const content = document.querySelector('.content');
+const allRow = document.querySelectorAll('.row');
+const numOfCol = document.querySelector('.row').querySelectorAll('.box');
+
+console.log(numOfCol.length);
+
+addRowBtn.addEventListener('click', function() {
+    const newRow = document.createElement('div');
+    newRow.setAttribute('class','row');
+
+
+    for(let i = 0; i < numOfCol.length; i++)
+    {
+        const newColumn = document.createElement('div');
+        newColumn.setAttribute('class','col-sm border border-dark box white');
+        newRow.appendChild(newColumn);
+        console.log(i);
+    }
+
+    console.log(newRow);
+    content.appendChild(newRow);
+});
